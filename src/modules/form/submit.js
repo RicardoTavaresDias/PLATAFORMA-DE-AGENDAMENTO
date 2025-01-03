767fbd6bff741f787146bdb5ef98c6fe3dc19739
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-01-03 11:23:24 -0300

    Importado module dayjs par formatação de data e configurado input data com a formatação data atual e data mínima para visualização sendo data atual.

---
+++ src/modules/form/submit.js
@@ -0,0 +1,19 @@
+
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const selectedDate = document.getElementById("date")
+
+// Date atual para formatar o input.
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+// Carrega a data atual e define a data mínima como sendo a data atual.
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit = (event) => {
+  // Previne o comportamento padão de carregar a página.
+  event.preventDefault()
+
+  console.log("ENVIADO!")
+}
