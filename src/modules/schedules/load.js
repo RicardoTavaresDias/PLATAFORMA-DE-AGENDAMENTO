6b358f9ca95508df08e0544dda30662b3d333be5
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-01-03 15:21:59 -0300

    Realizado recebimento da API os dados já cadastrador no Service

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,22 +1,19 @@
 
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load"
 
 // Seleciona o input de data.
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay() {
+export async function schedulesDay() {
   // Obtém a data do input.
   const date = selectedDate.value
 
+  // Busca na API os agendamentos
+  const dailySchesules = await scheduleFetchByDay({ date })
+  console.log(dailySchesules)
   // Renderiza as horas disponíveis.
   hoursLoad({ date })
 }
 
 
-
-
-
-
-// Buscar na API os agendamentos para carregar do lado direto da tela.
-
-// Os horários disponiveis (horário futuro + não agendado) do lado esquerdo (form)
