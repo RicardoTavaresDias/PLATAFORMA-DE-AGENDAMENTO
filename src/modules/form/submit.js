6ab8210b6a0a58faa889a0c049736b00d33effd6
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-01-03 15:03:59 -0300

    Server.json recebendo os dados e foi realizadon manipulação no submit para receber os dados e enviar

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,8 @@
 
 import dayjs from "dayjs"
 
+import { scheduleNew } from "../../services/schedule-new.js"
+
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
@@ -12,7 +14,7 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 selectedDate.value = inputToday
 selectedDate.min = inputToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   // Previne o comportamento padão de carregar a página.
   event.preventDefault()
 
@@ -40,7 +42,11 @@ form.onsubmit = (event) => {
     // Gera um ID
     const id = new Date().getTime()
 
-    console.log({id, name, when})
+    await scheduleNew({
+      id, 
+      name, 
+      when
+    })
 
   } catch (error) {
     alert("Não foi possível realizar o agendamento.")
